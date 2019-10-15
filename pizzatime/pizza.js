5d806eddf7b2210258caf4224ac4a338c0e587e7
balazs996 <55079352+balazs996@users.noreply.github.com>
2019-10-15 15:18:29 +0200

    Add files via upload

--- pizzatime/pizza.js
+++ pizzatime/pizza.js
@@ -1,1 +1,37 @@
 "use strict"
+
+let payment = 0;
+let order = document.getElementById("order");
+
+function send() {
+    let type = document.forms["form"]["type"].value;
+    let size = document.forms["form"]["size"].value;
+    let writeType = "";
+    switch (type) {
+        case "mar":
+            writeType = "Margherita"
+            break;
+        case "haw":
+            writeType = "Hawaii"
+            break;
+        case "hun":
+            writeType = "Magyaros"
+            break;
+    }
+    let writeSize = "";
+    switch (size) {
+        case "small":
+            writeSize = "kicsi"
+            break;
+        case "medium":
+            writeSize = "közepes"
+            break;
+        case "large":
+            writeSize = "nagy"
+            break;
+    }
+
+    let text = `<p>Az ön redelése: 1 db ${writeSize} ${writeType} pizza. </p>` ;
+    text += `<p>Fizetendő összeg: ${payment}Ft</p>` ;
+    order.innerHTML = text;
+}
